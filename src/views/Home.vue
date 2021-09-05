bbc42e600c2bf8646d6a1ae940a10afcc7ed3660
Yoshihisa <nagabayashi.y@icloud.com>
2021-09-05 11:19:20 +0900

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home flex">
+    <CryptList v-for="(data, index) in lists" :key="index" :Value="data.Value" :Description="data.Description" />
+  </div>
+</template>
+
+<script>
+import CryptList from "../components/CryptList";
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          Value: "BTC",
+          Description: "ビットコイン(現物取引)",
+        },
+      ]
+    };
+  },
+  components: {
+    CryptList
+  }
+};
+</script>
+
+<style scoped>
+body {
+  background: #eee;
+}
+.flex {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
